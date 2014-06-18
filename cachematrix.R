f962fe8f05dffa1d0b425cf1d14b8682ed5d6b72
lena <lena.carlsson@staff.balticspot.com>
2014-06-18 15:34:22 +0200

    assignment 2 in R programming (lc)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## ProgrammingAssignment 2 in R Programming
+## How to cache an evaluated value and re-use it when needed
 
-## Write a short comment describing this function
 
+## Takes a matrix and caches its inverse 
 makeCacheMatrix <- function(x = matrix()) {
 
+	# Deklarerar resultatmatrisen som NULL
+	res <- NULL
+	
+	# Funktionen 'setM' sparar inkommande matris som x och
+	# nollställer reultatmatrisen.
+	setM <- function(y) { 
+	      x  <<- y 
+	      res <<- NULL 
+	}
+
+	# Funktionen 'getM' returnerar ett matrisobjekt
+	getM <- function() x
+
+	# Funktionen 'setInverseMatrix' inverterar resultatmatrisen och sparar
+	setInverseM <- function(solve) res <<- solve
+	
+	# Funktionen 'getInverseMatrix' returnerar den inverterade resultatmatrisen
+	getInverseM <- function() res
+
+	# Lista på interna funktioner
+	list(setM=setM, 
+	     getM=getM,
+	     setInverseM = setInverseM,
+	     getInverseM = getInverseM)
 }
 
 
-## Write a short comment describing this function
 
+## Takes a matrix and returns its inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+	   # Hämta den inverterade versionen av inkommande x-matris
+	   res <- x$getInverseM()
+	   # Om den finns, dvs om res inte är tom,
+	   if (! is.null(res)) {
+	      # har vi hittat en cachad matrix
+	      message("Found cached matrix")
+	      # returnera den
+	      return(res)
+	   }
+	
+	   # Om matrisen INTE fanns sparad,
+	   # skapa nytt matrisobjekt av x
+	   newM <- x$getM()
+	   # invertera objektet,
+	   res <- solve(newM,...)
+	   # spara det
+	   x$setInverseM(res)
+	   # och returnera det
+	   res
 }
